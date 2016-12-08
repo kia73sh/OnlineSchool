84c87c7f164458280639c370ceb2b0e117a18907
kiarash <kia.scarlet@yahoo.com>
2016-12-08 11:23:34 +0330

    first commit

--- app/src/main/java/com/sourcey/materiallogindemo/Http/Clients/OnResponseListener.java
+++
@@ -1,8 +0,0 @@
-package com.sourcey.materiallogindemo.Http.Clients;
-
-import java.util.Objects;
-
-public interface OnResponseListener {
-    void onSuccess(Object[] objects);
-    void onFailed(int responseCode);
-}
---
+++ app/src/main/java/com/sourcey/materiallogindemo/Models/LoginModel.java
@@ -0,0 +1,8 @@
+package com.sourcey.materiallogindemo.Models;
+
+/**
+ * Created by kiarash on 12/8/2016 AD.
+ */
+
+public class LoginModel {
+}
